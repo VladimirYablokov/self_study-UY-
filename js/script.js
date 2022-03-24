188d956c669120314166a39d99d49dda1e2004bc
Vladimir Yablokov <YablokovWork@gmail.com>
2022-03-24 16:27:42 +0300

    valid and cycle added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,10 +8,36 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('один из послелдних просмотренных фильмов?', ''),
-      b = prompt('на сколько оцените его?', ''),
-      c = prompt('один из послелдних просмотренных фильмов?', ''),
-      d = prompt('на сколько оцените его?', '');
+// const a = prompt('один из послелдних просмотренных фильмов?', ''),
+//       b = prompt('на сколько оцените его?', ''),
+//       c = prompt('один из послелдних просмотренных фильмов?', ''),
+//       d = prompt('на сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for(let i = 0; i < 2; i++){
+    const a = prompt('один из послелдних просмотренных фильмов?', ''),
+          b = prompt('на сколько оцените его?', '');
+
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }else{
+        console.log('error');
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10){
+    console.log('Просмотрено мало фильмов');
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    console.log('вы классный зритель');
+}else if(personalMovieDB.count > 30){
+    console.log('вы киноман');
+}else{
+    console.log('произошла ошибка');
+}
+
+console.log(personalMovieDB);
